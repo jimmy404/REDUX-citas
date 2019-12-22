96dca1140bcabc2a4612f087807190c897de065b
Jimmy <bruno_pier@hotmail.com>
2019-12-21 23:29:48 -0300

    Working on validation

--- src/reducers/validacionReducer.js
+++ src/reducers/validacionReducer.js
@@ -2,3 +2,14 @@ const initialState = {
     error: false
 }
 
+export default function(state= initialState, action) {
+    switch(action.type) {
+        case 'VALIDAR_FORMULARIO':
+            return{
+                ...state,
+                error: action.payload
+            }
+            default:
+                return state;
+    }
+}
