3fc36f270d833770bf162eecb77fbb7402c62799
Jimmy <bruno_pier@hotmail.com>
2019-12-22 00:29:04 -0300

    Finished project

--- src/store.js
+++ src/store.js
@@ -1,16 +1,22 @@
 import { createStore } from 'redux';
 import reducer from './reducers';
-
+import {obtenerStateStorage, guardarStateStorage} from './localstorage';
 //Definir state inicial
 
 //const initialState = [];
 
+const storageState = obtenerStateStorage();
 
 const store = createStore(
     reducer,
-    //initialState,
+    storageState,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-
 );
+
+store.subscribe( () => {
+    guardarStateStorage({
+        citas: store.getState().citas
+    })
+} )
 
 export default store;
