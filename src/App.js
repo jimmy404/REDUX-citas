cd09146118364fa7cb33f6d824ad63019d5e6556
Jimmy <bruno_pier@hotmail.com>
2019-12-21 22:56:57 -0300

    Working on AgregarCita form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
-
+import AgregarCita from './componentes/AgregarCita';
+//redux
+import store from './store';
+import { Provider } from 'react-redux';
 
 function App() {
   return (
+    <Provider store={store}>
     <div className="container">
       <header>
         <h1 className="text-center">Administrador de pacientes de veterinaria</h1>
@@ -10,13 +14,14 @@ function App() {
 
       <div className="row mt-3">
             <div className="col-md-6">
-              Formulario Aqui
+              <AgregarCita />
             </div>
             <div className="col-md-6">
               Listado Aqui
             </div>
           </div>
     </div>
+    </Provider>
   );
 }
 
