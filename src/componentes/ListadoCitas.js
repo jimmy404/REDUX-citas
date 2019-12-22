fba403fe521288e205c6002fcc15eeffa09fbb34
Jimmy <bruno_pier@hotmail.com>
2019-12-22 00:15:48 -0300

    Working on delete button in ListadoCitas

--- src/componentes/ListadoCitas.js
+++ src/componentes/ListadoCitas.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {useSelector} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
+import {borrarCitaAction} from '../actions/citasActions'
 
 const ListadoCitas = () => {
 
@@ -9,13 +10,16 @@ const ListadoCitas = () => {
     //mensaje condicional
     const mensaje = Object.keys(citas.citas).length === 0 ? 'No Hay Citas' : 'Administra las Citas aqui';
 
+    //dispatch para mandar llamar la accion de eliminar
+    const dispatch = useDispatch();
+
     return(
         <div className="card mt-5">
             <div className="card-body">
                 <h2 className="card-title text-center">{mensaje}</h2>
                 <div className="lista-citas">
                     {citas.citas.map(cita=>(
-                        <div className="media mt-3">
+                        <div key={cita.id} className="media mt-3">
                             <div className="media-body">
                                 <h3 className="mt-0"> {cita.mascota}</h3>
                                 <p className="card-text"><span>Nombre Dueño:</span> {cita.propietario}</p>
@@ -23,7 +27,9 @@ const ListadoCitas = () => {
                                 <p className="card-text"><span>Hora:</span> {cita.hora}</p>
                                 <p className="card-text"><span>Sintomas:</span> <br /> {cita.sintomas}</p>
                                 <button
-                                    className="btn btn-danger">Borrar &times;
+                                    className="btn btn-danger"
+                                    onClick={() => dispatch(borrarCitaAction(cita.id))}
+                                    >Borrar &times;
                                 </button>
                             </div>
                         </div>
