042e6381c787b06b2d18fe5da7d3a911fc737999
Jimmy <bruno_pier@hotmail.com>
2019-12-21 22:57:17 -0300

    Working on AgregarCita form in AgregarCita.js

--- src/store.js
+++ src/store.js
@@ -1,12 +1,14 @@
 import { createStore } from 'redux';
+import reducer from './reducers';
 
 //Definir state inicial
 
-const initialState = [];
+//const initialState = [];
+
 
 const store = createStore(
     reducer,
-    initialState,
+    //initialState,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
 );
